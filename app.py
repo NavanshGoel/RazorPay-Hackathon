d16affeaa54d427072b68da774516876ab60e66e
Tejaswi-Kumar <tejpatna24@gmail.com>
2021-12-03 19:01:57 +0530

    updation in top grossing section

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 import razorpay
 from datetime import datetime
 import time
+import requests
+import json
+import math
 
 app = Flask(__name__)
 app.secret_key = 'secret101'
@@ -12,9 +15,42 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/forgot-password.html')
+def forgot_password():
+    return render_template('forgot-password.html')
+
+
+@app.route('/register.html')
+def register():
+    return render_template('register.html')
+
+
 @app.route('/dashboard.html')
 def dashboard():
     return render_template('dashboard.html')
+
+
+@app.route("/seller", methods=['GET', 'POST'])
+def home():
+    req = requests.get(
+        'https://raw.githubusercontent.com/ashank2603/RazorPay-Hackathon/main/items.json')
+    data = json.loads(req.content)
+    return render_template('seller.html', data=data['items'])
+
+
+@app.route("/cart", methods=['GET', 'POST'])
+def send_data():
+    if request.method == 'POST':
+        cust_name = request.form.get("cust_name")
+        cust_phone = request.form.get("cust_phone")
+        cust_email = request.form.get("cust_email")
+        selected_item = request.form.get("itemCheck")
+    return render_template('cart.html', cust_name=cust_name, cust_phone=cust_phone, cust_email=cust_email, selected_item=selected_item)
 
 
 @app.route('/profile.html')
@@ -31,19 +67,27 @@ def topgrossing():
     d = dict()
     for i in js['items']:
         for j in i['line_items']:
+            num_days = calcDays(i['date'])
+            if num_days <= 30:
+                quant_last_30_days = j['quantity']
+            else:
+                quant_last_30_days = 0
+
             if j['name'] not in d:
-                ts = i['date']
-                curr = int(time.time())
-                g = ts-curr
-                if g > 0:
-                    continue
-                f = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d')
                 d[j['name']] = [j['description'], j['amount'],
-                                j['quantity'], f, j['net_amount']*j['quantity']/100]
+                                j['quantity'], quant_last_30_days, j['net_amount']*j['quantity']/100]
             else:
                 d[j['name']][2] += j['quantity']
+                d[j['name']][2] += quant_last_30_days
                 d[j['name']][4] += j['net_amount']*j['quantity']/100
     return render_template('topgrossing.html', d=d)
+
+
+def calcDays(ts):
+    curr = int(time.time())
+    g = ts-curr
+    num_days = math.floor(g/1000/60/60/24)
+    return num_days
 
 
 @app.errorhandler(404)
